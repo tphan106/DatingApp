7440e4ad1428541160d1b86c0623018a9a70dbe3
tphan106 <tphan7591@gmail.com>
2020-09-15 20:10:53 -0700

    Update Backend

---
+++ DatingApp.API/Dtos/UserForRegister.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegister
+    {
+        [Required]
+        public string Username{get; set;}
+        [Required]
+        [StringLength(8, MinimumLength=4, ErrorMessage= "Must meet the requirement")]
+        public string Password { get; set; }
+    }
+}
